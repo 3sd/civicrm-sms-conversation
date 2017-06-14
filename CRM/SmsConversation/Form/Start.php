256ba82b9640bf9794055dc9ce16f726bf4ee8f4
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-14 23:06:39 +0100

    summary action for starting an SMS conversation with a single contact

---
+++ CRM/SmsConversation/Form/Start.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Used for starting an SMS conversation with a SINGLE contact
+ *
+ * @see https://wiki.civicrm.org/confluence/display/CRMDOC/QuickForm+Reference
+ */
+class CRM_SmsConversation_Form_Start extends CRM_Core_Form {
+
+  public function preProcess(){
+    $this->contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this);
+    $contact = civicrm_api3('Contact', 'getsingle', ['id' => $this->contactId]);
+    CRM_Utils_System::setTitle(ts('Start an SMS conversation with %1', [1 => $contact['display_name']]));
+
+  }
+
+  public function setDefaultValues() {
+    list($defaults['send_at'], $defaults['send_at_time']) = CRM_Utils_Date::setDateDefaults(date('Y-m-d H:i:s'), 'activityDateTime');
+
+    return $defaults;
+  }
+
+
+  public function buildQuickForm() {
+
+    // Conversation selector
+    $this->addEntityRef('conversation_id', ts('Conversation'), [
+      'entity' => 'SmsConversation',
+      'api' => ['label_field' => 'id'],
+      'placeholder' => ts('- Select conversation -'),
+      'select' => ['minimumInputLength' => 0]
+    ], TRUE);
+
+    // Choose a time for this
+    $this->addDateTime('send_at', ts('Send at'), TRUE);
+
+    $this->addButtons(array(
+      array(
+        'type' => 'cancel',
+        'name' => ts('Cancel'),
+        'isDefault' => TRUE,
+      ),
+      array(
+        'type' => 'submit',
+        'name' => ts('Send'),
+        'isDefault' => TRUE,
+      ),
+    ));
+
+    // export form elements
+    parent::buildQuickForm();
+  }
+
+
+  public function postProcess() {
+    $values = $this->exportValues();
+    $params['contact_id'] = $this->contactId;
+    $params['conversation_id'] = $values['conversation_id'];
+    $result = civicrm_api3('SmsConversation', 'start', $params);
+    CRM_Core_Session::setStatus($result);
+    parent::postProcess();
+  }
+
+}
