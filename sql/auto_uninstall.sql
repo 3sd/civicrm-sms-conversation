3f0eeaeee285eef9042ff36b840d5141faea2857
Matthew Wire <mjw@mjwconsult.co.uk>
2017-06-14 14:57:57 +0100

    Rearrange drop tables for uninstall

--- sql/auto_uninstall.sql
+++ sql/auto_uninstall.sql
@@ -1,4 +1,4 @@
+DROP TABLE IF EXISTS `civicrm_sms_conversation_action`;
+DROP TABLE IF EXISTS `civicrm_sms_conversation_contact`;
 DROP TABLE IF EXISTS `civicrm_sms_conversation`;
 DROP TABLE IF EXISTS `civicrm_sms_conversation_question`;
-DROP TABLE IF EXISTS `civicrm_sms_conversation_contact`;
-DROP TABLE IF EXISTS `civicrm_sms_conversation_action`;
