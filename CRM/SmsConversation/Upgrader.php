369848341becfc375b660beff74c8d2f2f7f5a3e
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-23 22:44:58 +0100

    typo in upgrade SQL

--- CRM/SmsConversation/Upgrader.php
+++ CRM/SmsConversation/Upgrader.php
@@ -11,7 +11,7 @@ class CRM_SmsConversation_Upgrader extends CRM_SmsConversation_Upgrader_Base {
   public function upgrade_4700() {
     // Set is_active to required, default 1
     $this->ctx->log->info('Applying update 4700');
-    CRM_Core_DAO::executeQuery("UPDATE `civicrm_sms_conversation` SET is_active = 1 WHERE is_active = NULL");
+    CRM_Core_DAO::executeQuery("UPDATE `civicrm_sms_conversation` SET is_active = 1 WHERE is_active IS NULL");
     CRM_Core_DAO::executeQuery("ALTER TABLE `civicrm_sms_conversation` CHANGE `is_active` `is_active` TINYINT(4) NULL DEFAULT '1' COMMENT 'Is this conversation enabled?'");
     return TRUE;
   }
