4d85926bd9242eccd61f029bcf189392bd52fbf1
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-07-04 12:15:39 +0100

    minimal commit to fix start conversation bug

--- CRM/SmsConversation/BAO/Contact.php
+++ CRM/SmsConversation/BAO/Contact.php
@@ -126,7 +126,7 @@ class CRM_SmsConversation_BAO_Contact extends CRM_SmsConversation_DAO_Contact {
 
     $convContact = CRM_SmsConversation_BAO_Contact::getCurrentConversation($contactId);
     // Ask the question
-    if (CRM_SmsConversation_BAO_Question::ask($question['id'], $contactId, $convContact)) {
+    if (CRM_SmsConversation_BAO_Question::ask($question['id'], $contactId, reset($convContact))) {
       return $convContact['id'];
     }
     else {
